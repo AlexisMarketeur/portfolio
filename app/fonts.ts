31e05369a56141affd4d35f145cce8236b755ba3
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-04-12 17:04:30 +0200

    init shadcnUI & framer motion

---
+++ app/fonts.ts
@@ -0,0 +1,4 @@
+import { Raleway, Poppins } from "next/font/google";
+
+export const raleway = Raleway({ subsets: ["latin"] });
+export const poppins = Poppins({ weight: "400", subsets: ["latin"] });
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { poppins } from "./fonts";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className="overflow-x-hidden" lang="en">
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
