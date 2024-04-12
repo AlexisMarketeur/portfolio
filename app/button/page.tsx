63b7c532df7e1c65068441f84220ffbec0057cc0
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-04-13 00:37:30 +0200

    init live

---
+++ app/button/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function page() {
+  return (
+    <div className="bg-red-500 h-screen w-screen overflow-hidden">
+      <Button variant="default">Hello</Button>
+    </div>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between w-full items-baseline">
+      <Button variant="link" size="default" asChild>
+        <Link href="/">ma méthode</Link>
+      </Button>
+      <Button variant="link" size="default" asChild>
+        <Link href="/">à propos</Link>
+      </Button>
+
+      <Button variant="link" asChild>
+        <Link href="https://calendly.com" target="_blank">
+          discutons !
+        </Link>
+      </Button>
+    </nav>
+  );
+};
+
+export default Navbar;
