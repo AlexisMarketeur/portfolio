e8f700385927f0e2cf68cb6750f402bdd5c091b7
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-04-14 19:06:52 +0200

    reponsive 1 hero

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Hero from "@/components/hero";
 export default function Home() {
   return (
     <>
-      <div className="mt-6 mb-96">{/* <Hero /> */}</div>
+      <div className="mt-6 mb-96">
+        <Hero />
+      </div>
     </>
   );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -4,45 +4,51 @@ import { raleway } from "@/app/fonts";
 
 export default function Hero() {
   return (
-    <header className="flex flex-col">
+    <header className="flex flex-col mt-20 md:mt-0">
       <div className="flex flex-row">
         <div
-          className={`font-medium relative text-4xl xl:text-[280px] xl:leading-[250px] tracking-tighter ${raleway.className}`}
+          className={`font-medium tracking-tighter leading-[0.84] ${raleway.className} w-max whitespace-nowrap`}
         >
-          <div className="overflow-hidden">
-            <p className="flex animate-slideUp1 opacity-0">
+          <div className="overflow-hidden w-full">
+            <p className="flex animate-slideUp1 opacity-0 text-[14vw]">
               <Image
                 src={glyph}
-                style={{ height: "auto", width: "200px" }}
                 alt="glyph"
                 priority
+                className="h-auto w-[10.3vw]"
               />
               FREELANCE
             </p>
           </div>
-          <div className="overflow-hidden">
-            <p className="flex animate-slideUp2 opacity-0">GOOGLE ADS</p>
+          <div className="overflow-hidden w-full">
+            <p className="flex animate-slideUp2 opacity-0  text-[14vw]">
+              GOOGLE ADS
+            </p>
           </div>
-          <div className="overflow-hidden">
-            <p className="flex animate-slideUp3 opacity-0">PARTNER</p>
+          <div className="relative w-full">
+            <div className="overflow-hidden">
+              <p className="flex animate-slideUp3 opacity-0 text-[14vw]">
+                PARTNER
+              </p>
+            </div>
+            <div className="absolute right-0 xl:right-10 top-0  animate-appears1 opacity-0">
+              <video
+                autoPlay
+                muted
+                loop
+                playsInline={true}
+                className="rounded-full w-[25vw] max-w-[350px] h-auto"
+              >
+                <source src="/memoji.webm" type="video/webm" />
+                <source src="/memoji.mp4" type="video/mp4" />
+                Votre navigateur ne supporte pas la balise vidéo.
+              </video>
+            </div>
           </div>
-        </div>
-        <div className="absolute bottom-20 right-60 animate-appears1 opacity-0">
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline={true}
-            className="rounded-full w-80 h-auto"
-          >
-            <source src="/memoji.webm" type="video/webm" />
-            <source src="/memoji.mp4" type="video/mp4" />
-            Votre navigateur ne supporte pas la balise vidéo.
-          </video>
         </div>
       </div>
-      <div>
-        <h1 className="text-4xl animate-appears1 opacity-0" id="methode">
+      <div className="flex justify-between relative w-full">
+        <h1 className="text-lg pt-12 md:text-2xl lg:text-4xl animate-appears1 opacity-0">
           Je vous fais gagner de l'argent avec{" "}
           <span className="text-red">Google Ads</span>
         </h1>
@@ -50,3 +56,5 @@ export default function Hero() {
     </header>
   );
 }
+
+// text-5xl sm:text-6xl xl:text-[280px] xl:leading-[250px]
