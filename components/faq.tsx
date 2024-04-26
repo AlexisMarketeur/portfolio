6c4f68f445fc337344c280c3757e021f43093bf3
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-04-26 19:02:28 +0200

    FAQ fix

--- components/faq.tsx
+++ components/faq.tsx
@@ -11,23 +11,99 @@ export function FAQ() {
       <article className="w-11/12 md:w-9/12">
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger>
+              Quels sont vos tarifs pour des campagnes Google Ads?
+            </AccordionTrigger>
             <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
+              La tarification de mes services dépend largement de vos besoins
+              spécifiques. Par exemple, une campagne simple pourrait coûter
+              moins cher qu'une campagne à grande échelle nécessitant une
+              optimisation et un suivi continu. Discutons ensemble pour que je
+              puisse comprendre vos objectifs et vous fournir une estimation
+              précise lors de notre premier appel.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionTrigger>
+              Pourquoi devrais-je choisir vos services plutôt que ceux d'une
+              agence?
+            </AccordionTrigger>
             <AccordionContent>
-              Yes. It comes with default styles that matches the other
-              components&apos; aesthetic.
+              Contrairement aux grandes agences, je vous offre une attention
+              personnalisée et un suivi dédié pour vos campagnes Google Ads. Mon
+              approche individualisée assure que vos objectifs spécifiques de
+              marketing sont atteints avec précision et efficacité.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
-            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionTrigger>
+              Quelle est la rapidité de mise en place d'une campagne Google Ads?
+            </AccordionTrigger>
             <AccordionContent>
-              Yes. It's animated by default, but you can disable it if you
-              prefer.
+              Une fois que nous aurons défini vos objectifs et que l'accord sera
+              conclu, je peux lancer votre campagne presque immédiatement. Selon
+              la complexité, la configuration initiale peut prendre de quelques
+              jours à une semaine.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger>
+              Comment se déroule la gestion de campagne avec vous?
+            </AccordionTrigger>
+            <AccordionContent>
+              Après avoir établi vos objectifs et budget, je configure et lance
+              vos campagnes. J'effectue ensuite des ajustements continus pour
+              optimiser les performances et le retour sur investissement. Vous
+              recevrez des rapports réguliers pour suivre les progrès réalisés.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-5">
+            <AccordionTrigger>
+              Que dois-je vous fournir pour démarrer une campagne Google Ads?
+            </AccordionTrigger>
+            <AccordionContent>
+              Pour débuter, j'aurai besoin de comprendre vos objectifs de
+              marketing, votre public cible, et le budget que vous souhaitez
+              allouer à Google Ads. Des informations sur vos produits ou
+              services sont également essentielles pour créer des annonces
+              pertinentes et ciblées.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-6">
+            <AccordionTrigger>
+              Facturez-vous pour les ajustements ou optimisations
+              supplémentaires?
+            </AccordionTrigger>
+            <AccordionContent>
+              Non, mes services incluent les ajustements nécessaires pour
+              optimiser vos campagnes sans frais supplémentaires. Mon objectif
+              est d'atteindre les meilleures performances possibles sans coûts
+              cachés.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-7">
+            <AccordionTrigger>
+              Offrez-vous des remboursements si je ne suis pas satisfait des
+              résultats?
+            </AccordionTrigger>
+            <AccordionContent>
+              Je n'offre pas de remboursements, mais je m'engage à ajuster et
+              optimiser les campagnes jusqu'à ce que vous soyez satisfait des
+              résultats. Si vous avez des préoccupations, nous pouvons commencer
+              par une petite campagne test pour évaluer la collaboration.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-8">
+            <AccordionTrigger>
+              Que se passe-t-il après la fin de la campagne ou si j'approuve les
+              résultats?
+            </AccordionTrigger>
+            <AccordionContent>
+              Si vous avez d'autres besoins en marketing digital ou souhaitez
+              étendre vos campagnes, nous pouvons discuter d'une collaboration
+              continue. Si la campagne répond à vos besoins immédiats, nous
+              pouvons conclure le projet, bien que je reste disponible pour tout
+              support ou ajustement futur.
             </AccordionContent>
           </AccordionItem>
         </Accordion>
--- components/softSkills.tsx
+++ components/softSkills.tsx
@@ -1,72 +1,84 @@
-import { Check } from "lucide-react";
+import {
+  Handshake,
+  Layers,
+  MessageCircle,
+  PencilLine,
+  Presentation,
+  Zap,
+} from "lucide-react";
 
 export const SoftSkills = () => (
-  <div className="flex w-full justify-center bg-backgroundSecondary">
+  <section className="flex w-full justify-center bg-backgroundSecondary">
     <div className="w-11/12 md:w-9/12">
       <div className="flex w-full flex-col items-start gap-4 ">
         <h2 className="text-left text-3xl font-bold uppercase md:text-6xl lg:text-7xl xl:text-8xl">
-          SOFT SKILLS
+          MES BONNES PRATIQUES{" "}
         </h2>
 
         <div className="flex w-full flex-col gap-10 pt-12">
-          <div className="grid grid-cols-2 items-start gap-10 lg:grid-cols-3">
-            <div className="flex w-full flex-col items-start gap-6 sm:flex-row">
-              <Check className="mt-2 h-4 w-4 text-primary" />
+          <div className="grid grid-cols-2 items-start gap-8 md:gap-10 lg:grid-cols-3">
+            <article className="flex w-full flex-col items-start gap-6 sm:flex-row">
+              <PencilLine className="mt-2 h-4 w-4 text-primary" />
               <div className="flex flex-col gap-1">
-                <p>Easy to use</p>
-                <p className="text-muted-foreground text-sm">
-                  We&apos;ve made it easy to use and understand.
+                <p>Créatif et innovant</p>
+                <p className="text-sm text-neutral-500">
+                  Je transforme les idées en solutions novatrices et
+                  captivantes.
                 </p>
               </div>
-            </div>
-            <div className="flex flex-col items-start gap-6 sm:flex-row">
-              <Check className="mt-2 h-4 w-4 text-primary" />
+            </article>
+            <article className="flex flex-col items-start gap-6 sm:flex-row">
+              <MessageCircle className="mt-2 h-4 w-4 text-primary" />
               <div className="flex flex-col gap-1">
-                <p>Fast and reliable</p>
-                <p className="text-muted-foreground text-sm">
-                  We&apos;ve made it fast and reliable.
+                <p>Communicatif et clair</p>
+                <p className="text-muted-foreground text-sm text-neutral-500">
+                  Je parle clairement, assurant une compréhension et
+                  coordination sans faille.
                 </p>
               </div>
-            </div>
-            <div className="flex flex-col items-start gap-6 sm:flex-row">
-              <Check className="mt-2 h-4 w-4 text-primary" />
+            </article>
+            <article className="flex flex-col items-start gap-6 sm:flex-row">
+              <Presentation className="mt-2 h-4 w-4 text-primary" />
               <div className="flex flex-col gap-1">
-                <p>Beautiful and modern</p>
-                <p className="text-muted-foreground text-sm">
-                  We&apos;ve made it beautiful and modern.
+                <p>Adaptable et flexible</p>
+                <p className="text-muted-foreground text-sm text-neutral-500">
+                  Je m'ajuste rapidement pour répondre aux besoins changeants de
+                  chaque projet.
                 </p>
               </div>
-            </div>
-            <div className="flex w-full flex-col items-start gap-6 sm:flex-row">
-              <Check className="mt-2 h-4 w-4 text-primary" />
+            </article>
+            <article className="flex w-full flex-col items-start gap-6 sm:flex-row">
+              <Layers className="mt-2 h-4 w-4 text-primary" />
               <div className="flex flex-col gap-1">
-                <p>Easy to use</p>
-                <p className="text-muted-foreground text-sm">
-                  We&apos;ve made it easy to use and understand.
+                <p>Organisé et méthodique</p>
+                <p className="text-muted-foreground text-sm text-neutral-500">
+                  Je planifie avec soin, garantissant efficacité et précision à
+                  chaque étape.
                 </p>
               </div>
-            </div>
-            <div className="flex flex-col items-start gap-6 sm:flex-row">
-              <Check className="mt-2 h-4 w-4 text-primary" />
+            </article>
+            <article className="flex flex-col items-start gap-6 sm:flex-row">
+              <Zap className="mt-2 h-4 w-4 text-primary" />
               <div className="flex flex-col gap-1">
-                <p>Fast and reliable</p>
-                <p className="text-muted-foreground text-sm">
-                  We&apos;ve made it fast and reliable.
+                <p>Proactif et diligent</p>
+                <p className="text-muted-foreground text-sm text-neutral-500">
+                  J'anticipe les défis pour maintenir une avance constante.
                 </p>
               </div>
-            </div>
-            <div className="flex flex-col items-start gap-6 sm:flex-row">
-              <Check className="mt-2 h-4 w-4 text-primary" />
+            </article>
+            <article className="flex flex-col items-start gap-6 sm:flex-row">
+              <Handshake className="mt-2 h-4 w-4 text-primary" />
               <div className="flex flex-col gap-1">
-                <p>Beautiful and modern</p>
-                <p className="text-muted-foreground text-sm">
-                  We&apos;ve made it beautiful and modern.
+                <p>Empathique et à l'écoute</p>
+                <p className="text-muted-foreground text-sm text-neutral-500">
+                  J'écoute activement, construisant des relations solides et
+                  basées sur la confiance.
                 </p>
               </div>
-            </div>
+            </article>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 );
