43d9d656d5036e64bf6cb464d624b9125f47d976
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-04-27 23:03:11 +0200

    stat change

--- components/statsSection.tsx
+++ components/statsSection.tsx
@@ -28,20 +28,7 @@ export default function StatsSection() {
               Google.
             </p>
           </div>
-          <div className="flex min-h-52 w-full flex-col lg:w-2/12">
-            <div className="mb-6">
-              <Search className="h-16 w-16 rotate-[-4deg] rounded-md border-2 border-b-4 border-black bg-gradient-to-l from-greenCustom-100 to-blueCustom-100 p-3 " />
-            </div>
-            <h3 className={`${inter.className} pb-4 text-8xl font-bold `}>
-              90%
-            </h3>
-            <p className="pb-2 text-lg font-bold ">Des consommateurs</p>
-            <p className="text-sm">
-              Estiment que les publicités Google Ads influencent leur recherche
-              et leur décision d’achat lorsqu'ils en effectuent une !
-            </p>
-          </div>
-          <div className="flex min-h-52 w-full flex-col lg:w-2/12">
+          <div className="flex min-h-52 w-full flex-col lg:w-[18%]">
             <div className="mb-6">
               <TrendingUp className="h-16 w-16 rotate-3 rounded-md border-2 border-b-4 border-black bg-gradient-to-r from-greenCustom-100 to-blueCustom-100 p-3 " />
             </div>
@@ -55,6 +42,19 @@ export default function StatsSection() {
               Si vous investissez un euro dans une publicité Google, vous pouvez
               vous attendre à gagner en moyenne 2 euros en retour grâce
               à ces publicités.
+            </p>
+          </div>
+          <div className="flex min-h-52 w-full flex-col lg:w-2/12">
+            <div className="mb-6">
+              <Search className="h-16 w-16 rotate-[-4deg] rounded-md border-2 border-b-4 border-black bg-gradient-to-l from-greenCustom-100 to-blueCustom-100 p-3 " />
+            </div>
+            <h3 className={`${inter.className} pb-4 text-8xl font-bold `}>
+              90%
+            </h3>
+            <p className="pb-2 text-lg font-bold ">Des consommateurs</p>
+            <p className="text-sm">
+              Estiment que les publicités Google Ads influencent leur recherche
+              et leur décision d’achat lorsqu'ils en effectuent une !
             </p>
           </div>
         </div>
