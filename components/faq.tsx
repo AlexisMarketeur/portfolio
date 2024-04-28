a37b5015c575305051fa5d1f5faa38a2b0694ee3
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-04-28 14:52:58 +0200

    Faq update

--- components/faq.tsx
+++ components/faq.tsx
@@ -12,73 +12,146 @@ export function FAQ() {
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
             <AccordionTrigger>
-              Quels sont vos tarifs pour des campagnes Google Ads?
+              Quels sont les avantages de Google Ads pour mon entreprise ?
             </AccordionTrigger>
             <AccordionContent>
-              La tarification de mes services dépend largement de vos besoins
-              spécifiques. Par exemple, une campagne simple pourrait coûter
-              moins cher qu'une campagne à grande échelle nécessitant une
-              optimisation et un suivi continu. Discutons ensemble pour que je
-              puisse comprendre vos objectifs et vous fournir une estimation
-              précise lors de notre premier appel.
+              La publicité sur Google Ads offre à votre entreprise une
+              visibilité immédiate auprès des utilisateurs recherchant des
+              produits ou services similaires aux vôtres.
+              <br />
+              <br />
+              Vous bénéficiez d'un ciblage précis selon la localisation, les
+              mots-clés et les centres d'intérêt de votre audience.
+              <br />
+              <br />
+              En plus vous avez un contrôle total sur votre budget publicitaire
+              et ne payez que lorsque quelqu'un clique sur votre annonce. Avec
+              des outils de mesure avancés, vous pouvez suivre le retour sur
+              investissement de vos campagnes et ajuster votre stratégie en
+              conséquence.
+              <br />
+              <br />
+              Cette flexibilité vous permet d'optimiser vos campagnes pour
+              obtenir les meilleurs résultats pour votre entreprise.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
             <AccordionTrigger>
+              Quels sont vos tarifs pour des campagnes Google Ads ?
+            </AccordionTrigger>
+            <AccordionContent>
+              Pour mes prestations Google Ads, mon taux journalier moyen est de
+              300 euros.
+              <br />
+              <br />
+              Je propose également des forfaits tarifaires pour l'optimisation
+              continue de la campagne sur la durée. <br />
+              <br />
+              Ces forfaits sont adaptés à vos besoins spécifiques et peuvent
+              être discutés en fonction de la complexité de votre projet et de
+              vos objectifs. <br />
+              <br />
+              N'hésitez pas à me contacter pour obtenir plus d'informations et
+              discuter des options disponibles pour votre entreprise.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>
               Pourquoi devrais-je choisir vos services plutôt que ceux d'une
-              agence?
+              agence ?
             </AccordionTrigger>
             <AccordionContent>
               Contrairement aux grandes agences, je vous offre une attention
               personnalisée et un suivi dédié pour vos campagnes Google Ads. Mon
               approche individualisée assure que vos objectifs spécifiques de
-              marketing sont atteints avec précision et efficacité.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>
-              Quelle est la rapidité de mise en place d'une campagne Google Ads?
-            </AccordionTrigger>
-            <AccordionContent>
-              Une fois que nous aurons défini vos objectifs et que l'accord sera
-              conclu, je peux lancer votre campagne presque immédiatement. Selon
-              la complexité, la configuration initiale peut prendre de quelques
-              jours à une semaine.
+              marketing sont atteints avec précision et efficacité. <br />
+              <br />
+              Ma flexibilité et ma réactivité me permettent d'adapter rapidement
+              mes stratégies en fonction des performances de vos campagnes et
+              des évolutions de votre entreprise, garantissant ainsi une réponse
+              rapide à vos besoins spécifiques. <br />
+              <br />
+              De plus, mes tarifs compétitifs vous offrent un excellent rapport
+              qualité-prix par rapport aux grandes agences. <br />
+              <br />
+              Enfin, je valorise la transparence dans ma communication et mes
+              actions, établissant ainsi une relation de confiance où chaque
+              aspect de votre campagne publicitaire est clairement compris et
+              discuté. En choisissant mes services, vous avez l'assurance d'une
+              attention personnalisée, d'une expertise spécialisée et d'une
+              collaboration transparente pour atteindre vos objectifs marketing
+              avec succès.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-4">
             <AccordionTrigger>
-              Comment se déroule la gestion de campagne avec vous?
+              Quelle est la rapidité de mise en place d'une campagne Google Ads
+              ?
             </AccordionTrigger>
             <AccordionContent>
-              Après avoir établi vos objectifs et budget, je configure et lance
-              vos campagnes. J'effectue ensuite des ajustements continus pour
-              optimiser les performances et le retour sur investissement. Vous
-              recevrez des rapports réguliers pour suivre les progrès réalisés.
+              La rapidité de mise en place d'une campagne Google Ads dépend de
+              plusieurs facteurs, notamment de la complexité de la campagne et
+              de la disponibilité des ressources nécessaires. <br />
+              <br />
+              En général, je m'efforce de mettre en place les campagnes dans les
+              plus brefs délais afin de commencer à générer du trafic qualifié
+              vers votre site web dès que possible.
+              <br />
+              <br />
+              Cependant, il est également important de prendre le temps
+              nécessaire pour planifier et configurer la campagne de manière à
+              garantir son efficacité et sa pertinence.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-5">
             <AccordionTrigger>
-              Que dois-je vous fournir pour démarrer une campagne Google Ads?
+              Combien de temps dure la mise en place d’une campagne Google Ads ?
             </AccordionTrigger>
             <AccordionContent>
-              Pour débuter, j'aurai besoin de comprendre vos objectifs de
-              marketing, votre public cible, et le budget que vous souhaitez
-              allouer à Google Ads. Des informations sur vos produits ou
-              services sont également essentielles pour créer des annonces
-              pertinentes et ciblées.
+              Concernant la durée de mise en place, une campagne Google Ads peut
+              prendre jusqu'à une semaine et demie, selon sa complexité et vos
+              exigences spécifiques.
+              <br />
+              <br />
+              Seules les journées de travail seront facturées pour cette
+              période, afin de garantir un coût transparent et équitable pour
+              mes services. Je m'engage à travailler efficacement pour minimiser
+              les délais et à vous tenir informé tout au long du processus afin
+              que vous puissiez commencer à bénéficier des avantages de votre
+              campagne dans les plus brefs délais.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-6">
             <AccordionTrigger>
-              Facturez-vous pour les ajustements ou optimisations
-              supplémentaires?
+              Que dois-je vous fournir pour démarrer une campagne Google Ads?
             </AccordionTrigger>
             <AccordionContent>
-              Non, mes services incluent les ajustements nécessaires pour
-              optimiser vos campagnes sans frais supplémentaires. Mon objectif
-              est d'atteindre les meilleures performances possibles sans coûts
-              cachés.
+              Pour démarrer une campagne Google Ads nous devons définir : <br />
+              <br />
+              L’objectifs de la campagne : Des détails sur les résultats que
+              vous souhaitez obtenir, comme la génération de leads ou
+              l'augmentation des ventes ou la notoriété.
+              <br />
+              <br />
+              Le budget : Le montant que vous prévoyez d'investir dans la
+              publicité, ainsi que toute restriction budgétaire.
+              <br />
+              <br />
+              Les mots-clés : Une liste des mots-clés pertinents selon vous pour
+              votre activité.
+              <br />
+              <br />
+              Les annonces : Le contenu des annonces que vous souhaitez diffuser
+              (photos, vidéos) ou des annonces références.
+              <br />
+              <br />
+              Le maximum d’informations sur votre entreprise : Des informations
+              sur votre entreprise, vos produits ou services, et votre marché
+              cible.
+              <br />
+              <br />
+              Accès à votre compte Google Ads : Si vous en avez déjà un,
+              fournissez-moi l'accès. Sinon, je peux vous aider à en créer un.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-7">
@@ -87,23 +160,20 @@ export function FAQ() {
               résultats?
             </AccordionTrigger>
             <AccordionContent>
-              Je n'offre pas de remboursements, mais je m'engage à ajuster et
-              optimiser les campagnes jusqu'à ce que vous soyez satisfait des
-              résultats. Si vous avez des préoccupations, nous pouvons commencer
-              par une petite campagne test pour évaluer la collaboration.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-8">
-            <AccordionTrigger>
-              Que se passe-t-il après la fin de la campagne ou si j'approuve les
-              résultats?
-            </AccordionTrigger>
-            <AccordionContent>
-              Si vous avez d'autres besoins en marketing digital ou souhaitez
-              étendre vos campagnes, nous pouvons discuter d'une collaboration
-              continue. Si la campagne répond à vos besoins immédiats, nous
-              pouvons conclure le projet, bien que je reste disponible pour tout
-              support ou ajustement futur.
+              Je ne propose pas de remboursements si vous n'êtes pas satisfait
+              des résultats, mais je m'engage à travailler en étroite
+              collaboration avec vous pour résoudre tout éventuel problème et
+              améliorer les performances de vos campagnes Google Ads.
+              <br />
+              <br />
+              Ma priorité est de vous fournir un service de qualité et de vous
+              aider à atteindre vos objectifs marketing. Si vous n'êtes pas
+              satisfait des résultats, nous pouvons discuter des ajustements
+              nécessaires pour améliorer les performances de vos campagnes.{" "}
+              <br />
+              <br />
+              Ma politique est de garantir votre satisfaction et de travailler
+              avec vous pour obtenir les meilleurs résultats possibles.
             </AccordionContent>
           </AccordionItem>
         </Accordion>
